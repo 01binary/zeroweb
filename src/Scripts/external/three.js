9cb5f15361d8629f409714316b9e3e47a831d474
01binary <valeriy.novytskyy@outlook.com>
2017-01-09 23:20:48 -0800

    Fix load order and global reference

--- src/Scripts/external/three.js
+++ src/Scripts/external/three.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(factory((global.THREE = global.THREE || {})));
+	(factory((window.THREE = window.THREE || {})));
 }(this, (function (exports) { 'use strict';
 
 	// Polyfills

446c8d89a4b545c37d3c44e25246a2ec6290e957
01binary <valeriy.novytskyy@outlook.com>
2021-03-11 08:07:06 -0800

    Minor cleanup

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,10 +3,6 @@ import { RouteComponentProps } from "@reach/router"
 import SEO from './SEO';
 import Header from './Header';
 
-interface ILayoutProps {
-    location: any
-}
-
 const Layout: FunctionComponent<RouteComponentProps> = ({
     children,
     location }) => (

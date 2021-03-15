f904bca4481251b1716f931e4c899565f0c95373
01binary <valeriy.novytskyy@outlook.com>
2021-03-14 22:35:41 -0700

    Updated title component

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,22 +1,57 @@
 import React, { FunctionComponent } from 'react';
 import styled from 'styled-components';
 
-const buttonWidth = 110;
-const buttonHeight = 50;
-const bulletWidth = 5;
-const bulletHeight = 20;
-const collectionOffsets = [ "articles", "projects", "about" ];
+const NAVLINK_WIDTH = 110;
+const NAVLINK_HEIGHT = 50;
+const BULLET_WIDTH = 5;
+const BULLET_HEIGHT = 20;
+const BULLET_TOP = Math.ceil(NAVLINK_HEIGHT / 2 - NAVLINK_HEIGHT * 0.1);
+const CALLOUT_TOP = Math.ceil(BULLET_TOP / 2) + 0.5;
+const COLLECTION_OFFSETS = [ "articles", "projects", "about" ];
 
-const Heading = styled.h1`
-    margin-top: 0;
+interface ICalloutProps {
+    offset: number,
+    className?: string
+};
 
-    @media (max-width: ${props => props.theme.mobile}) {
-        margin-left: ${props => props.theme.spacingHalf};
-    }
-`;
+const getGeometry = (
+    points: Array<Array<number>>
+): string => points
+    .reduce((acc, cur, index) => 
+        acc +
+            (index ? ' L ' : '') +
+            cur[0] + ' ' + cur[1]
+    , 'M ');
 
-const Graphic = styled.svg`
+const Callout: React.FunctionComponent<ICalloutProps> = ({
+    offset,
+    className
+}) => (
+    <svg
+        className={className}
+        width={offset}
+        height={NAVLINK_HEIGHT}
+    >
+        <path d={getGeometry([
+            [ 0, BULLET_TOP ],
+            [ 0, BULLET_TOP + BULLET_HEIGHT ],
+            [ BULLET_WIDTH, BULLET_TOP + BULLET_HEIGHT ],
+            [ BULLET_WIDTH, BULLET_TOP + 3 ]]) + 'z'}
+            className="title-bullet"
+        />
+        <path d={getGeometry([
+            [ 0.5, BULLET_TOP + 1 ],
+            [ 0.5, CALLOUT_TOP ],
+            [ offset - BULLET_TOP / 2, CALLOUT_TOP ],
+            [ offset - 1, 0 ]])}
+            className="title-callout"
+        />
+    </svg>
+);
+
+const Decorator = styled(Callout)`
     margin-bottom:-8px;
+    margin-right: -${props => props.offset - 16}px;
 
     .title-bullet {
         fill: ${props => props.theme.primaryColor};
@@ -33,14 +68,13 @@ const Graphic = styled.svg`
     }
 `;
 
-const getGeometry = (
-    points: Array<Array<number>>
-): string => points
-    .reduce((acc, cur, index) => 
-        acc +
-            (index ? ' L ' : '') +
-            cur[0] + ' ' + cur[1]
-    , 'M ');
+const Heading = styled.h1`
+    margin-top: 0;
+
+    @media (max-width: ${props => props.theme.mobile}) {
+        margin-left: ${props => props.theme.spacingHalf};
+    }
+`;
 
 interface ITitleProps {
     collection: string
@@ -49,35 +83,11 @@ interface ITitleProps {
 const Title: FunctionComponent<ITitleProps> = ({
     collection,
     children
-}) => {
-    const bulletTop = Math.ceil(buttonHeight / 2 - buttonHeight * 0.1);
-    const calloutTop = Math.ceil(bulletTop / 2) + 0.5;
-    const left = buttonWidth * (collectionOffsets.indexOf(collection) + 1);
-
-    return (
-        <Heading>
-            <Graphic
-                width={left}
-                height={buttonHeight}
-            >
-                <path d={getGeometry([
-                    [ 0, bulletTop ],
-                    [ 0, bulletTop + bulletHeight ],
-                    [ bulletWidth, bulletTop + bulletHeight ],
-                    [ bulletWidth, bulletTop + 3 ]]) + 'z'}
-                    className="title-bullet"
-                />
-                <path d={getGeometry([
-                    [ 0.5, bulletTop + 1 ],
-                    [ 0.5, calloutTop ],
-                    [ left - bulletTop / 2, calloutTop ],
-                    [ left - 1, 0 ]])}
-                    className="title-callout"
-                />
-            </Graphic>
-            {children}
-        </Heading>
-    );
-};
+}) => (
+    <Heading>
+        <Decorator offset={NAVLINK_WIDTH * (COLLECTION_OFFSETS.indexOf(collection) + 1)} />
+        {children}
+    </Heading>
+);
 
 export default Title;

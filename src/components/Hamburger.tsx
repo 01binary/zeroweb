32d50b1117fd47543369a3009e438e207ecdebe1
01binary <valeriy.novytskyy@outlook.com>
2021-03-30 12:55:15 +0000

    Thinner hamburger

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -18,27 +18,6 @@ const HamburgerLabel = styled.label.attrs(
     display: none;
   
     @media (max-width: ${props => props.theme.mobile}) {
-      transition: opacity ${props => props.theme.animationFast} ease-out;
-      opacity: .6;
-  
-      .fill-foreground {
-        fill: ${props => props.theme.foregroundColor} !important;
-      }
-
-      .stroke-foreground {
-        stroke: ${props => props.theme.foregroundColor} !important;
-      }
-  
-      &:hover {
-        opacity: 1;
-        .fill-foreground {
-          fill: ${props => props.theme.focusColor};
-        }
-        .stroke-foreground {
-          stroke: ${props => props.theme.focusColor};
-        }
-      }
-  
       display: block;
       position: fixed;
       top: 0;
@@ -62,21 +41,21 @@ const HamburgerIcon = styled.svg`
         animation: loop 1s linear infinite;
         
         @keyframes loop {
-        0% {
-            transform: translateX(0);
-        }
+            0% {
+                transform: translateX(0);
+            }
 
-        25% {
-            transform: translateX(-8px);
-        }
+            25% {
+                transform: translateX(-8px);
+            }
 
-        50% {
-            transform: translateX(-24px);
-        }
+            50% {
+                transform: translateX(-24px);
+            }
 
-        100% {
-            transform: translateX(-48px);
-        }
+            100% {
+                transform: translateX(-48px);
+            }
         }
     }
 
@@ -88,8 +67,8 @@ const HamburgerIcon = styled.svg`
         opacity: ${props => props.menuOpen ? 1 : 0};
     }
 
-    .wave, .fr {
-        transition: opacity .3s ease-out;
+    .menu {
+        transition: opacity ${props => props.theme.animationFast} ease-out;
         opacity: ${props => props.menuOpen ? 0 : 1};
     }
 
@@ -116,13 +95,11 @@ const Hamburger: FunctionComponent<IHamburgerProps> = ({
         <HamburgerLabel menuOpen={menuOpen} onClick={() => showMenu(!menuOpen)}>
         <HamburgerIcon width="24px" height="24px" viewBox="0 0 24 24" menuOpen={menuOpen}>
             <g className="clipped">
-            <path className="fill-foreground wave" d="M60.6,10.2C57.5,8.1,54.3,6,48,6s-9.5,2.1-12.6,4.2c-3,2-5.7,3.8-11.4,3.8s-8.5-1.9-11.4-3.8C9.5,8.1,6.3,6,0,6v2
-                c5.7,0,8.5,1.9,11.4,3.8c3.1,2,6.3,4.2,12.6,4.2s9.5-2.1,12.6-4.2c3-2,5.7-3.8,11.4-3.8s8.5,1.9,11.4,3.8c3.1,2,6.3,4.2,12.6,4.2v-2
-                C66.3,14,63.5,12.1,60.6,10.2z"/>
-            <rect className="fill-foreground fr" y="2" width="24" height="2"/>
-            <rect className="fill-foreground fr" y="18" width="24" height="2"/>
-            <line className="stroke-foreground close-first" strokeWidth="2.5" x1="22" y1="2" x2="2" y2="22"/>
-            <line className="stroke-foreground close-second" strokeWidth="2.5" x1="2" y1="2" x2="22" y2="22"/>
+                <path className="menu wave stroke-foreground fill-none" d="M0,7c12,0,12,8,24,8c12,0,12-8,24-8s12,8,24,8"/>
+                <line className="menu stroke-foreground fill-none" x1="0" y1="3.5" x2="24" y2="3.5"/>
+                <line className="menu stroke-foreground fill-none" x1="0" y1="19.5" x2="24" y2="19.5"/>
+                <line className="close-first stroke-foreground" strokeWidth="1" x1="22" y1="2" x2="2" y2="22"/>
+                <line className="close-second stroke-foreground" strokeWidth="1" x1="2" y1="2" x2="22" y2="22"/>
             </g>
         </HamburgerIcon>
         </HamburgerLabel>

f3a95ead4544d86b83bab0bf3e716aeee2fe1f94
01binary <valeriy.novytskyy@outlook.com>
2021-04-19 19:05:25 -0700

    Initial pagination

---
+++ src/articles/testpagesthree.md
@@ -0,0 +1,26 @@
+---
+title: Hello, world! Three
+description: This is a test
+image: ./images/test.png
+date: 2021-02-10
+tags: ["engineering-mechanical"]
+---
+
+Some smarty pants
+
+> "Hello world, he said"
+
+Some inline code
+
+```javascript
+const text = 'World'
+console.log(`Hello ${text}`)
+```
+
+And of course a table
+
+|Heading One|Heading Two|Heading Three|
+|-|-|-|
+|Data One|Data Two|Data Three|
+|Data Four|Data Five|Data Six|
+|Data Seven|Data Eight|Data Nine|
--- src/components/PostIndex.tsx
+++ src/components/PostIndex.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useContext } from 'react';
+import { graphql, Link } from 'gatsby';
 import BlogContext from '../components/BlogContext';
 import { getTagFilter, getTagDescriptionById } from '../components/TagList';
 import Title from '../components/Title';
@@ -6,21 +7,31 @@ import PostList from '../components/PostList';
 import Summary from '../components/Summary';
 import { AllPostsQuery } from '../models/AllPostsQuery';
 
-export interface IndexQuery {
-  data: AllPostsQuery;
+interface IndexPageContext {
+  collection: string,
+  numberOfPages: number,
+  pageNumber: number,
+  humanPageNumber: number,
+  previousPagePath: string,
+  nextPagePath: string
 };
 
-interface IndexProps extends IndexQuery {
-  collection: string;
+interface IndexQuery {
+  data: AllPostsQuery;
+  pageContext: IndexPageContext;
 };
 
 /**
  * Index page for articles and projects
- * @param props - The gatsby graphql data source. 
+ * @param props - The index page query.
  * @returns The index page content.
  */
-const PostIndex: FC<IndexProps> = ({
-  collection,
+const PostIndex: FC<IndexQuery> = ({
+  pageContext: {
+    collection,
+    nextPagePath,
+    previousPagePath
+  },
   data: {
     allMdx: {
       nodes,
@@ -37,12 +48,55 @@ const PostIndex: FC<IndexProps> = ({
       <Title collection={collection}>
         {collection[0].toUpperCase() + collection.substr(1)}
       </Title>
+
       {tagId &&
         <Summary>All <b>{tagDescription}</b> {collection}:</Summary>
       }
+
       <PostList nodes={nodes} group={group} />
+
+      <div>
+        <Link to={previousPagePath}>Previous</Link>
+        <Link to={nextPagePath}>Next</Link>
+      </div>
     </main>
   );
 };
+
+export const pageQuery = graphql`
+  query($collection: String!, $skip: Int, $limit: Int, $tag: String) {
+    allMdx
+    (
+      sort: { fields: [frontmatter___date], order: DESC },
+      skip: $skip,
+      limit: $limit,
+      filter: {
+        fields: {
+          collection: { eq: $collection },
+          tags: { eq: $tag }
+        }
+      }
+    ) {
+      nodes {
+        slug
+        timeToRead
+        frontmatter {
+          title,
+          relativeDate: date(fromNow: true)
+          date(formatString: "MMMM DD, YYYY")
+        }
+        fields {
+          url,
+          collection
+          tags
+        }
+      }
+      group(field: frontmatter___tags) {
+        tag: fieldValue
+        totalCount
+      }
+    }
+  }
+`
 
 export default PostIndex;

2c138204ac2169c918041db051fadd59cddc8acb
01binary <valeriy.novytskyy@outlook.com>
2021-05-19 19:27:32 -0700

    Auth only for GraphQL API

--- api-server/resolvers.js
+++ api-server/resolvers.js
@@ -1,11 +1,10 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 const {
   getComments,
   getComment,
   addComment,
   deleteComment,
   editComment,
-  voteComment
 } = require('./database');
 
 exports.resolvers = {
@@ -14,45 +13,110 @@ exports.resolvers = {
       return getComments(slug);
     },
 
-    comment: async (root, { slug, timestamp }) => getComment(slug, timestamp),
+    comment: async (root, { slug, timestamp }) => {
+      return getComment(slug, timestamp);
+    },
   },
   Mutation: {
     addComment: async (
       root,
-      { comment },
-      { user }
+      {
+        comment: {
+          slug,
+          parentTimestamp,
+          markdown,
+          reaction,
+          paragraph,
+          rangeStart,
+          rangeEnd
+        }
+      },
+      { user },
     ) => {
-      if (!user?.authenticated) throw new AuthenticationError();
-      addComment({
-        ...comment,
-        user,
-        timestamp: new Date().toISOString()
+      console.log('addComment mutation got user', user);
+
+      if (!user?.authenticated) throw new AuthenticationError(
+        'must be logged in with a social provider to add comments'
+      );
+
+      return addComment({
+        slug,
+        timestamp: new Date().toISOString(),
+        userId: user.id,
+        parentTimestamp,
+        markdown,
+        reaction,
+        votes: 0,
+        paragraph,
+        rangeStart,
+        rangeEnd
       });
     },
 
-    deleteComment: async (
-      root,
-      { comment },
-      { user }
-    ) => {
-      if (!user?.authenticated) throw new AuthenticationError();
-      return deleteComment(comment);
-    },
-
     editComment: async (
-      root, { comment }, { user }
+      root,
+      { comment: { slug, timestamp, markdown, reaction } },
+      { user },
     ) => {
-      if (!user?.authenticated) throw new AuthenticationError();
-      return editComment(comment);
+      if (!user?.authenticated) throw new AuthenticationError(
+        'must be logged in with a social provider to edit comments'
+      );
+
+      const original = await getComment(slug, timestamp);
+
+      if (original.userId !== user.id) throw new UserInputError(
+        'users can only edit their own comments'
+      );
+
+      return editComment(
+        {
+          slug,
+          timestamp,
+          markdown,
+          reaction,
+        },
+        original
+      );
     },
 
     voteComment: async (
       root,
-      { comment: { slug, timestamp, upvote, downvote } },
-      { user }
+      { comment: { slug, timestamp, upVote, downVote } },
+      { user },
     ) => {
-      if (!user?.authenticated) throw new AuthenticationError();
-      return voteComment(slug, timestamp, upvote ? 1 : downvote ? -1 : 0);
-    }
+      if (!user?.authenticated) throw new AuthenticationError(
+        'must be logged in with a social provider to vote on comments'
+      );
+
+      const original = await getComment(slug, timestamp);
+      const vote = upVote ? 1 : downVote ? -1 : 0;
+
+      if (vote === 0) throw new UserInputError(
+        'comments can be either upvoted or downvoted'
+      );
+
+      return voteComment(
+        { slug, timestamp, votes: original.votes + vote },
+        original
+      );
+    },
+
+    deleteComment: async (
+      root,
+      { comment: { slug, timestamp } },
+      { user },
+    ) => {
+      if (!user?.authenticated) throw new AuthenticationError(
+        'must be logged in to delete comments'
+      );
+
+      const { userId: originalUserId } = await getComment(slug, timestamp);
+
+      if (originalUserId !== user.id) throw new UserInputError(
+        'users can only delete their own comments'
+      );
+
+      return deleteComment({ slug, timestamp });
+    },
   }
 };

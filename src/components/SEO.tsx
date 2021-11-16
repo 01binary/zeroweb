55aef8209ab7e2c185b861d1fd8a5f2d4d0793d3
01binary <valeriy.novytskyy@outlook.com>
2021-11-15 18:50:13 -0800

    Another font stack fix

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -64,7 +64,9 @@ const SEO: FC<Partial<SiteMetadataQuery>> = ({
       <meta name="twitter:description" content={description || siteDesc} />
       <meta name="twitter:image" content={image || siteImage} />
 
-      <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"/>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="1" />
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;1,400&display=swap" rel="stylesheet" />
     </Helmet>
   );
 };

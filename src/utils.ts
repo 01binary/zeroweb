059370c8c0034fe8068cf2fdb73173e0213bad90
01binary <valeriy.novytskyy@outlook.com>
2021-11-28 15:27:50 -0800

    Refactor post utilities

---
+++ src/utils.ts
@@ -0,0 +1,35 @@
+/*--------------------------------------------------------*\
+|  ██████   ██  |
+|  ██  ██   ██  |
+|  ██  ██   ██  |
+|  ██████   ██  |  binary : tech art
+|
+|  Formatting and data processing utilities.
+|----------------------------------------------------------
+|  Copyright(C) 2021 Valeriy Novytskyy
+\*---------------------------------------------------------*/
+
+import { getHeadingSlug, getHeadingUrl } from "./components/Heading";
+import HeadingQuery from "./types/HeadingQuery";
+
+export const slugifyHeadings = (baseUrl: string, headings: HeadingQuery[]) =>
+  headings.map((heading) => {
+    const slug = getHeadingSlug(false, heading.value);
+    return {
+      ...heading,
+      url: getHeadingUrl(baseUrl, slug),
+      slug,
+    };
+  });
+
+export const getDateValue = (relativeDate: string): string =>
+  relativeDate.split(" ")[0];
+
+export const getDateUnits = (relativeDate: string): string =>
+  relativeDate.split(" ").slice(1).join(" ");
+
+export const openUrl = (url, params) => {
+  const href = new URL(url);
+  href.search = new URLSearchParams(params).toString();
+  window.open(href.toString());
+};

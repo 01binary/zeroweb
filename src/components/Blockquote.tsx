2160d1b467a61cc61bc3873bf7a1bf056b3791ee
01binary <valeriy.novytskyy@outlook.com>
2021-11-29 15:55:21 +0000

    Fix ruler marks vibrating on resize

--- src/components/Blockquote.tsx
+++ src/components/Blockquote.tsx
@@ -1,27 +1,15 @@
-import React, { FC } from 'react';
-import styled from 'styled-components';
+import React, { FC } from "react";
+import styled from "styled-components";
 
 const Quote = styled.blockquote`
-  margin-right: -9px;
-  padding-right: 0;
+  margin: 0;
+  padding: 0;
 
   p {
-    margin-left: 0;
-    padding-left: 0;
-  }
-
-  @media (max-width: ${props => props.theme.mobile}) {
-    body {
-      margin-right: initial;
-      padding-right: initial;
-    }
+    padding-left: 1em;
   }
 `;
 
-const Blockquote: FC = ({ children }) => (
-  <Quote>
-    {children}
-  </Quote>
-);
+const Blockquote: FC = ({ children }) => <Quote>{children}</Quote>;
 
 export default Blockquote;

3bff7d4dd4935b9c8988842374019eca8fb22ab9
01binary <valeriy.novytskyy@outlook.com>
2021-11-29 12:55:21 -0800

    Fix ruler marks vibrating on resize

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -9,20 +9,27 @@
 |  Copyright(C) 2021 Valeriy Novytskyy
 \*---------------------------------------------------------*/
 
-import React, { FC, useState, useRef, useCallback, useMemo, useEffect } from 'react';
-import styled from 'styled-components';
-import Highlight, { defaultProps } from 'prism-react-renderer';
-import { useStyledDarkMode } from 'gatsby-styled-components-dark-mode';
-import light from 'prism-react-renderer/themes/github';
-import dark from 'prism-react-renderer/themes/vsDark';
-import { useTooltipController, useTooltipTarget } from '../hooks/useTooltip';
-import { Tooltip, Arrow } from './Tooltip';
-import { RULER_ENDMARK_WIDTH } from './Ruler';
-import CopyIcon from '../images/copy.svg';
-import LightIcon from '../images/light.svg';
-import DarkIcon from '../images/dark.svg';
+import React, {
+  FC,
+  useState,
+  useRef,
+  useCallback,
+  useMemo,
+  useEffect,
+} from "react";
+import styled from "styled-components";
+import Highlight, { defaultProps } from "prism-react-renderer";
+import { useStyledDarkMode } from "gatsby-styled-components-dark-mode";
+import light from "prism-react-renderer/themes/github";
+import dark from "prism-react-renderer/themes/vsDark";
+import { useTooltipController, useTooltipTarget } from "../hooks/useTooltip";
+import { Tooltip, Arrow } from "./Tooltip";
+import { RULER_ENDMARK_WIDTH } from "./Ruler";
+import CopyIcon from "../images/copy.svg";
+import LightIcon from "../images/light.svg";
+import DarkIcon from "../images/dark.svg";
 
-const DARK_MODE_OVERRIDE = 'darkCode';
+const DARK_MODE_OVERRIDE = "darkCode";
 
 type CodeWrapperProps = {
   isDark: boolean;
@@ -31,24 +38,37 @@ type CodeWrapperProps = {
 
 const CodeWrapper = styled.div<CodeWrapperProps>`
   position: relative;
-  margin-left: ${props => props.theme.spacingHalf};
-  margin-right: ${props => props.theme.spacingHalf};
-  width: calc(130% + ${RULER_ENDMARK_WIDTH}px + ${props => props.theme.spacing} + 3px);
+  margin-left: ${(props) => props.theme.spacingHalf};
+  margin-right: calc(
+    -30% - ${RULER_ENDMARK_WIDTH}px - ${(props) => props.theme.borderThick} - ${(props) => props.theme.spacingOneAndHalf}
+  );
+  padding-right: calc(
+    30% + ${RULER_ENDMARK_WIDTH}px + ${(props) => props.theme.borderThick} +
+      ${(props) => props.theme.spacingOneAndHalf}
+  );
 
   .stroke-foreground {
-    stroke: ${props => props.isCodeDark ? (props.isDark ? props.theme.foregroundColor : props.theme.backgroundColor) : (props.isDark ? props.theme.backgroundColor : props.theme.foregroundColor)};
+    stroke: ${(props) =>
+      props.isCodeDark
+        ? props.isDark
+          ? props.theme.foregroundColor
+          : props.theme.backgroundColor
+        : props.isDark
+        ? props.theme.backgroundColor
+        : props.theme.foregroundColor};
   }
 
   &:after {
-    content: '';
+    content: "";
     position: absolute;
-    right: ${props => props.theme.spacingOneAndHalf};
     top: 0;
-    width: calc(${props => props.theme.border} * 1.5);
+    right: ${(props) => props.theme.spacingOneAndHalf};
+    width: calc(${(props) => props.theme.border} * 1.5);
     height: 100%;
-    background: ${props => props.theme.foregroundColor};
-    opacity: .4;
-    transition: opacity ${props => props.theme.animationFast} ease-out;
+    background: ${(props) => props.theme.foregroundColor};
+    opacity: 0.4;
+    transition: opacity ${(props) => props.theme.animationFast} ease-out;
+    z-index: -1;
   }
 
   &:hover {
@@ -57,7 +77,7 @@ const CodeWrapper = styled.div<CodeWrapperProps>`
     }
   }
 
-  @media (max-width: ${props => props.theme.mobile}) {
+  @media (max-width: ${(props) => props.theme.mobile}) {
     width: initial;
 
     &:after {
@@ -72,9 +92,8 @@ const CodeWrapper = styled.div<CodeWrapperProps>`
 
 const Pre = styled.pre`
   position: relative;
-  font-size: ${props => props.theme.smallFontSize};
-  padding: ${props => props.theme.spacingHalf};
-  width: calc(67% - ${props => props.theme.spacingHalf});
+  font-size: ${(props) => props.theme.smallFontSize};
+  padding: ${(props) => props.theme.spacingHalf};
 
   &:hover {
     button {
@@ -82,7 +101,7 @@ const Pre = styled.pre`
     }
   }
 
-  @media (max-width: ${props => props.theme.mobile}) {
+  @media (max-width: ${(props) => props.theme.mobile}) {
     width: initial;
   }
 `;
@@ -95,8 +114,9 @@ const ToolButton = styled.button<ToolButtonProps>`
   position: absolute;
   width: 32px;
   height: 32px;
-  top: ${props => props.theme.spacingHalf};
-  right: ${({ offset, theme }) => offset === 0 ? theme.spacingHalf : theme.spacingDouble};
+  top: ${(props) => props.theme.spacingHalf};
+  right: ${({ offset, theme }) =>
+    offset === 0 ? theme.spacingHalf : theme.spacingDouble};
   border: none;
   cursor: pointer;
   fill: none;
@@ -104,7 +124,7 @@ const ToolButton = styled.button<ToolButtonProps>`
   -moz-appearance: none;
   background: none;
   opacity: 0;
-  transition: opacity ${props => props.theme.animationFast} ease-out;
+  transition: opacity ${(props) => props.theme.animationFast} ease-out;
 `;
 
 type CodeButtonProps = {
@@ -125,20 +145,18 @@ const CodeButton: FC<CodeButtonProps> = ({
   hideTip,
   onClick,
 }) => {
-  const {
-    showTip: showTargetTip,
-    targetRef
-  } = useTooltipTarget({
+  const { showTip: showTargetTip, targetRef } = useTooltipTarget({
     tooltipElement: tipRef.current,
     showTip,
     verticalOffsetDesktop: 10,
     verticalOffsetMobile: 10,
-    placement: 'top'
+    placement: "top",
   });
 
-  const showTooltip = useCallback(
-    () => showTargetTip(tipId),
-  [showTargetTip, tipId]);
+  const showTooltip = useCallback(() => showTargetTip(tipId), [
+    showTargetTip,
+    tipId,
+  ]);
 
   return (
     <ToolButton
@@ -154,19 +172,19 @@ const CodeButton: FC<CodeButtonProps> = ({
 };
 
 const Code: FC = ({ children }) => {
-  const darkOverride = localStorage.getItem(DARK_MODE_OVERRIDE) == '1';
+  const darkOverride = localStorage.getItem(DARK_MODE_OVERRIDE) == "1";
 
   const snippetRef = useRef<HTMLElement>(null);
   const { isDark } = useStyledDarkMode();
-  const [ isCopied, setCopied ] = useState(false);
-  const [ isCodeDark, setCodeDark ] = useState(isDark || darkOverride);
+  const [isCopied, setCopied] = useState(false);
+  const [isCodeDark, setCodeDark] = useState(isDark || darkOverride);
 
   const {
     showTip,
     hideTip,
     tooltipText: tipId,
     tipProps,
-    tipRef
+    tipRef,
   } = useTooltipController();
 
   const copyCode = useCallback(() => {
@@ -185,7 +203,7 @@ const Code: FC = ({ children }) => {
     try {
       if (isDark === isCodeDark) {
         // Override dark mode
-        localStorage.setItem(DARK_MODE_OVERRIDE, '1');
+        localStorage.setItem(DARK_MODE_OVERRIDE, "1");
       } else {
         // Remove dark mode override
         localStorage.removeItem(DARK_MODE_OVERRIDE);
@@ -195,65 +213,66 @@ const Code: FC = ({ children }) => {
     }
 
     setCodeDark(!isCodeDark);
-
   }, [isDark, isCodeDark, setCodeDark]);
 
-  const className = (children as React.ReactElement)?.props?.className || '';
+  const className = (children as React.ReactElement)?.props?.className || "";
   const matches = className.match(/language-(?<lang>.*)/);
 
   return (
     <CodeWrapper isDark={isDark} isCodeDark={isCodeDark}>
       <Highlight
         {...defaultProps}
-        theme={(isDark || isCodeDark) ? dark : light}
+        theme={isDark || isCodeDark ? dark : light}
         code={(children as React.ReactElement).props.children}
         language={
-        matches && matches.groups && matches.groups.lang
-          ? matches.groups.lang
-          : ''
+          matches && matches.groups && matches.groups.lang
+            ? matches.groups.lang
+            : ""
         }
       >
         {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <Pre className={className} style={style}>
-          <span ref={snippetRef}>
-            {tokens
-              .filter((line) => line.length === 1 && line[0].empty ? false: true)
-              .map((line, i) => (
-                <div {...getLineProps({ line, key: i })}>
-                  {line.map((token, key) => (
-                    <span {...getTokenProps({ token, key })} />
-                  ))}
-                </div>
-            ))}
-          </span>
-          <CodeButton
-            offset={0}
-            tipId={'copy'}
-            tipRef={tipRef}
-            onClick={copyCode}
-            showTip={showTip}
-            hideTip={hideTip}
-          >
-            <CopyIcon />
-          </CodeButton>
-          {isDark === false &&
+          <Pre className={className} style={style}>
+            <span ref={snippetRef}>
+              {tokens
+                .filter((line) =>
+                  line.length === 1 && line[0].empty ? false : true
+                )
+                .map((line, i) => (
+                  <div {...getLineProps({ line, key: i })}>
+                    {line.map((token, key) => (
+                      <span {...getTokenProps({ token, key })} />
+                    ))}
+                  </div>
+                ))}
+            </span>
             <CodeButton
-              offset={1}
-              tipId={'mode'}
+              offset={0}
+              tipId={"copy"}
               tipRef={tipRef}
-              onClick={toggleDarkMode}
+              onClick={copyCode}
               showTip={showTip}
               hideTip={hideTip}
             >
-              {isCodeDark ? <LightIcon /> : <DarkIcon />}
+              <CopyIcon />
             </CodeButton>
-          }
-          <Tooltip {...tipProps} role="tooltip">
-            {tipId === 'copy' && (isCopied ? 'copied!' : 'copy code')}
-            {tipId === 'mode' && (isCodeDark ? 'light theme' : 'dark theme')}
-            <Arrow />
-          </Tooltip>
-        </Pre>
+            {isDark === false && (
+              <CodeButton
+                offset={1}
+                tipId={"mode"}
+                tipRef={tipRef}
+                onClick={toggleDarkMode}
+                showTip={showTip}
+                hideTip={hideTip}
+              >
+                {isCodeDark ? <LightIcon /> : <DarkIcon />}
+              </CodeButton>
+            )}
+            <Tooltip {...tipProps} role="tooltip">
+              {tipId === "copy" && (isCopied ? "copied!" : "copy code")}
+              {tipId === "mode" && (isCodeDark ? "light theme" : "dark theme")}
+              <Arrow />
+            </Tooltip>
+          </Pre>
         )}
       </Highlight>
     </CodeWrapper>

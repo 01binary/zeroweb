50939550fc0558313b088e7a952f1f37f427d3a3
01binary <valeriy.novytskyy@outlook.com>
2022-03-12 15:57:40 +0000

    Fix highlights that start or end at paragraph boundary

--- src/components/Paragraph/Paragraph.tsx
+++ src/components/Paragraph/Paragraph.tsx
@@ -66,26 +66,47 @@ const ParagraphHighlight: FC<{
   end: number;
 }> = ({ nodes, start, end }) => {
   // Insert <mark> into span that may contain both text and HTML nodes
-  const __html = nodes.reduce((html, { tag, text, pos, length }) => {
+  const __html = nodes.reduce((html, { tag, text, pos, length }, index) => {
     if (start <= pos) {
       if (end < pos + length) {
-        // End of highlight splits this node in half=
-        const before = text.substring(0, end - pos);
-        const after = text.substring(end - pos);
-        if (tag) return `${html}<${tag}>${before}</MARK>${after}</${tag}>`;
-        else return `${html}${before}</MARK>${after}`;
+        // Highlight ends in this node
+        if (start === 0) {
+          // Highlight begins at the start of the node
+          const highlight = text.substring(0, end - pos + 1);
+          const after = text.substring(end - pos + 1);
+          return tag
+            ? `${html}<${tag}><MARK>${highlight}</MARK>${after}</${tag}>`
+            : `${html}<MARK>${highlight}</MARK>${after}`;
+        } else {
+          // End of highlight splits this node
+          const before = text.substring(0, end - pos);
+          const after = text.substring(end - pos);
+          return tag
+            ? `${html}<${tag}>${before}</MARK>${after}</${tag}>`
+            : `${html}${before}</MARK>${after}`;
+        }
       } else {
         // Node does not interact with the highlight (appears before, after, or completely inside)
-        if (tag) return `${html}<${tag}>${text}</${tag}>`;
-        else return `${html}${text}`;
+        return tag ? `${html}<${tag}>${text}</${tag}>` : `${html}${text}`;
       }
     } else if (start > pos && start < pos + length) {
-      // Start of highlight splits this node in half
+      // Start of highlight splits this node
       const before = text.substring(0, start - pos);
-      const after = text.substring(start - pos);
-      if (tag)
-        return `${html}<${tag}>${before}</${tag}><MARK><${tag}>${after}</${tag}>`;
-      else return `${html}${before}<MARK>${after}`;
+
+      if (index + 1 === nodes.length) {
+        // Highlight ends in this node
+        const highlight = text.substring(start - pos, end - pos + 1);
+        const after = text.substring(end - pos + 1);
+        return tag
+          ? `${html}<${tag}>${before}<MARK>${highlight}</MARK>${after}</${tag}>`
+          : `${html}${before}<MARK>${highlight}</MARK>${after}`;
+      } else {
+        // Highlight ends in the next node
+        const after = text.substring(start - pos);
+        return tag
+          ? `${html}<${tag}>${before}</${tag}><MARK><${tag}>${after}</${tag}>`
+          : `${html}${before}<MARK>${after}`;
+      }
     }
   }, '');
 
--- src/projects/elysium-exoskeleton.md
+++ src/projects/elysium-exoskeleton.md
@@ -1,9 +1,20 @@
 ---
 title: Elysium Exoskeleton
-description: Exoskeleton prop from the movie Elysium
+description: A full-scale replica of the exoskeleton frame worn in the movie Elysium (2013) by Max Da Costa.
 image: ./images/elysium-exoskeleton.png
 date: 2021-02-11
 tags: ['engineering-mechanical', 'design-industrial', 'tool-inventor']
 ---
 
-Exosuit from the movie Elysium
+A full-scale replica of the exoskeleton frame worn in the movie Elysium (2013) by Max Da Costa.
+
+Created in Autodesk Inventor using prop auction photographs and screen captures. Many of the parts were with Protolabs and re-cast them out of Aluminum 356, others created using CNC. machined pistons on a metal lathe, and sewed a harness. The project was completed fall 2019, and now I'm working on tweaks to make it more comfortable to wear, such as re-casting some key parts out of hard rubber.
+
+## build
+
+See the [GitHub repository](https://github.com/01binary/elysium-max-exoskeleton), the [Hackaday project](https://hackaday.io/project/19830-elysium-max-exoskeleton/) or the [RPF build log](http://www.therpf.com/showthread.php?t=212832) for details.
+
+## instructions
+
+- [Assembly Instructions - Default (Light)](https://github.com/01binary/elysium-max-exoskeleton/raw/master/instructions/assembly%20instructions.pdf)
+- [Assembly Instructions - Hackaday (Dark)](https://github.com/01binary/elysium-max-exoskeleton/raw/hackadayinstructions/instructions/assembly%20instructions.pdf)

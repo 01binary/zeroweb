ae7e9779ddf69c3b71bd8112d3e92f4d2f934fe4
01binary <valeriy.novytskyy@outlook.com>
2022-03-13 04:37:13 +0000

    Fix errors on disappearing headings

--- src/hooks/useActiveHeading.ts
+++ src/hooks/useActiveHeading.ts
@@ -12,26 +12,26 @@
 import { useEffect, useState } from 'react';
 import HeadingQuery from '../types/HeadingQuery';
 
-const useActiveHeading = (
-  headings: Array<HeadingQuery>
-): string => {
+const useActiveHeading = (headings: Array<HeadingQuery>): string => {
   const [activeHeadingId, setActiveHeadingId] = useState('');
 
   useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) setActiveHeadingId(entry.target.id);
-        });
-      }
-    );
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) setActiveHeadingId(entry.target.id);
+      });
+    });
 
-    headings.forEach(heading =>
-      observer.observe(document.getElementById(heading.slug)));
+    headings.forEach((heading) => {
+      const headingEl = document.getElementById(heading.slug);
+      if (headingEl) observer.observe(headingEl);
+    });
 
-    return () => headings.forEach(heading =>
-      observer.unobserve(document.getElementById(heading.slug)));
-
+    return () =>
+      headings.forEach((heading) => {
+        const headingEl = document.getElementById(heading.slug);
+        if (headingEl) observer.unobserve(headingEl);
+      });
   }, [headings]);
 
   return activeHeadingId;

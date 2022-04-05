5f49fe62236270e94f0bd487e1db5114527b0585
01binary <valeriy.novytskyy@outlook.com>
2022-04-04 21:04:39 -0700

    Update google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,9 +23,28 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: '261858673',
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          `G-38HNLDCS3F`
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: [],
+          // Defaults to https://www.googletagmanager.com
+          origin: "https://www.01binary.us",
+        },
       },
     },
     {

300b23a7458b5a912ee0dd7800fe3670c7c54893
01binary <valeriy.novytskyy@outlook.com>
2022-04-16 13:48:29 +0000

    Remove quote icon

--- src/components/Post/Blockquote.tsx
+++ src/components/Post/Blockquote.tsx
@@ -1,38 +1,18 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
-import QuoteIcon from '../../images/quote.svg';
-
-const StyledQuoteIcon = styled(QuoteIcon)`
-  position: absolute;
-  top: 0;
-  left: ${(props) => props.theme.spacingHalf};
-`;
 
 const Quote = styled.blockquote`
   position: relative;
   margin: 0;
   padding: 0;
 
-  & > svg {
-    fill: ${(props) =>
-      props.theme.isDark
-        ? props.theme.borderColor
-        : props.theme.accentLightShadowColor};
-  }
-
   & > section > p {
-    padding-top: ${(props) => props.theme.spacingHalf};
-    padding-left: ${(props) => props.theme.spacingOneAndHalf};
+    padding-left: ${(props) => props.theme.spacing};
     font-weight: 300;
     font-style: italic;
   }
 `;
 
-const Blockquote: FC = ({ children }) => (
-  <Quote>
-    <StyledQuoteIcon />
-    {children}
-  </Quote>
-);
+const Blockquote: FC = ({ children }) => <Quote>{children}</Quote>;
 
 export default Blockquote;

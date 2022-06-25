7fbc174cb3068a27c00999915fbdbfc97194f42a
01binary <valeriy.novytskyy@outlook.com>
2022-06-25 14:26:53 +0000

    Tweak escalator icon

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -12,7 +12,7 @@
 import React, { FC, useCallback } from 'react';
 import styled from 'styled-components';
 import { MOBILE_WIDE, NARROW_SIDE_COMMENTS } from '../constants';
-import StairsIcon from '../images/escalator.svg';
+import EscalatorIcon from '../images/escalator.svg';
 
 // Approximate percentage of page read before showing scroll to top button
 const SCROLL_TO_TOP_THRESHOLD = 0.18;
@@ -114,7 +114,7 @@ const ScrollToTop: FC<{ readPosition: number; inline: boolean }> = ({
       visible={readPosition > SCROLL_TO_TOP_THRESHOLD}
       onClick={handleClick}
     >
-      <StairsIcon />
+      <EscalatorIcon />
       <ScrollButtonLabel inline={inline}>Going up?</ScrollButtonLabel>
     </Wrapper>
   );
